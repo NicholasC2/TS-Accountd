ee7f52f2e815094259a66bdbef8ae59cd6520950
NicholasC2 <nicholasc2@proton.me>
2026-04-02 19:59:05 +1300

    fixed session

--- validation_rules.ts
+++ validation_rules.ts
@@ -1,6 +1,6 @@
 export const validation_rules = {
     USERNAME: /^[a-zA-Z0-9_-]+$/,
-    PASSWORD: /^(?=.*[A-Za-z])(?=.*\d).{6,}$/
+    PASSWORD: /^(?=.*[A-Za-z])(?=.*\d).{6,}$/,
 };
 
 export const scrypt_options = { N: 16384, r: 8, p: 1 };
