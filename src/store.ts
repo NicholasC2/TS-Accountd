aeee5ebab30a1607d3e21c9b8de19220866323c0
NicholasC2 <nicholasc2@proton.me>
2026-04-03 14:39:28 +1300

    added gitignore and fixed store

--- src/store.ts
+++ src/store.ts
@@ -15,6 +15,26 @@ export class AccountStore {
     get(username: string): Account | undefined {
         return this.accounts.get(username);
     }
+
+    toJSON(): object[] {
+        const accounts: object[] = [];
+
+        this.accounts.forEach(account => {
+            accounts.push(account.toJSON(true));
+        })
+
+        return accounts;
+    }
+
+    static fromJSON(data: []): AccountStore {
+        const accountStore = new AccountStore();
+
+        data.forEach((account)=>{
+            accountStore.add(Account.fromJSON(account))
+        })
+
+        return accountStore;
+    }
 }
 
 export class SessionStore {
@@ -41,5 +61,25 @@ export class SessionStore {
 
     get(id: string): Session | undefined {
         return this.sessions.get(id);
+    }
+
+    toJSON(): object[] {
+        const sessions: object[] = [];
+
+        this.sessions.forEach(session => {
+            sessions.push(session.toJSON());
+        })
+
+        return sessions;
+    }
+
+    static fromJSON(data: []): SessionStore {
+        const sessionStore = new SessionStore();
+
+        data.forEach((session)=>{
+            sessionStore.add(Session.fromJSON(session))
+        })
+
+        return sessionStore;
     }
 }
